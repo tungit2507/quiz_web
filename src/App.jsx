13b0a1371aa9aed73399e921e12549ea6c9c756e
Hoang Thanh Tung <131683573+tungit2507@users.noreply.github.com>
2025-05-27 16:54:34 +0700

    remove bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useRoutes } from 'react-router-dom';
 import routes from './routers/index';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const element = useRoutes(routes);
