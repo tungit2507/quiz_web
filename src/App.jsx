a8cef6a7cfeb42f759d30fe36455adb37a282dca
Hoang Thanh Tung <131683573+tungit2507@users.noreply.github.com>
2025-05-27 17:00:11 +0700

    config tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useRoutes } from 'react-router-dom';
 import routes from './routers/index';
 import './App.css';
+import "tailwindcss";
 
 function App() {
   const element = useRoutes(routes);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 function BasicExample() {
     return (
-        <></>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-500">
+                Tailwind CSS đang hoạt động 🎉
+            </h1>
+        </div>
     );
 }
 
